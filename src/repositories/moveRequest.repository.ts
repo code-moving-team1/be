99401a8db917fcfd029a1c209830f6b8300d6d0b
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-09-18 20:32:13 +0900

    db 스키마 수정 + 일반 이사 요청 리스트 레포 #16

--- src/repositories/moveRequest.repository.ts
+++ src/repositories/moveRequest.repository.ts
@@ -1,4 +1,5 @@
 // src/repositories/moveRequest.repository.ts
+import { MoveRequestStatus, ServiceType, Prisma } from "@prisma/client";
 import { prisma } from "../lib/prisma";
 import { CreateMoveRequestInput } from "../schemas/moveRequest.schema";
 
@@ -11,5 +12,34 @@ export const createMoveRequest = async (
       ...data,
       customerId,
     },
+  });
+};
+
+interface GetMoveRequestsListParams {
+  limit: number;
+  page: number;
+  serviceType?: ServiceType;
+  orderBy?: "recent" | "deadline";
+}
+
+export const getMoveRequestsList = async ({
+  limit = 5,
+  page = 1,
+  serviceType,
+  orderBy = "recent",
+}: GetMoveRequestsListParams) => {
+  const whereConditions: Prisma.MoveRequestWhereInput = {
+    status: MoveRequestStatus.ACTIVE,
+    ...(serviceType && { serviceType }),
+  };
+
+  const orderByConditions: Prisma.MoveRequestOrderByWithRelationInput =
+    orderBy === "deadline" ? { moveDate: "asc" } : { createdAt: "desc" };
+
+  return prisma.moveRequest.findMany({
+    where: whereConditions,
+    orderBy: orderByConditions,
+    take: limit,
+    skip: (page - 1) * limit,
   });
 };
