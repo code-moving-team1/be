84fd6b825de5270909d00dc96c6dd6d955d8ab4e
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-09-19 13:37:01 +0900

    스키마 수정

--- src/schemas/moveRequest.schema.ts
+++ src/schemas/moveRequest.schema.ts
@@ -8,7 +8,9 @@ export const createMoveRequestSchema = z.object({
     message: "이사 날짜는 오늘 이후여야 합니다.",
   }),
   departure: z.string().min(2, "출발지를 입력해주세요."),
+  departureRegion: z.string(),
   destination: z.string().min(2, "도착지를 입력해주세요."),
+  destinationRegion: z.string(),
 });
 
 export type CreateMoveRequestInput = z.infer<typeof createMoveRequestSchema>;
