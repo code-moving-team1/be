d360f88bda26c717e195795408d37bb84fe68bf7
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-10-01 18:20:20 +0900

    지정 견적 요청 생성 및 수락 기초 기능 #10

---
+++ src/routes/directQuoteRequest.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import directQuoteRequestController from "../controllers/directQuoteRequest.controller";
+import { verifyAuth } from "../middlewares/auth";
+
+const router = Router();
+
+router.post("/", directQuoteRequestController.create);
+router.patch("/:id/accept", directQuoteRequestController.updateToAccepted);
+
+export default router;
