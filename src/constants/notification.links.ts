2b7a5d2ad035dd34fcdf08768860b7ad98ee3bb9
Woojin <jwj3171@gmail.com>
2025-11-05 16:16:55 +0900

    fix(notiLink): 견적생성됨 알림 클릭시 경로 수정

--- src/constants/notification.links.ts
+++ src/constants/notification.links.ts
@@ -1,7 +1,9 @@
 // src/constants/notification.links.ts
 
 export const notificationLink = {
-  newQuoteReceived: (moveRequestId: number) => `/myEstimates/${moveRequestId}`, // 고객
+  // newQuoteReceived: (moveRequestId: number) => `/myEstimates/${moveRequestId}`, // 고객
+  // Zustand 이슈로 바로 진입이 안되어 경로수정
+  newQuoteReceived: (moveRequestId: number) => `/myEstimates?tab=active`, // 고객
   quoteAccepted: (_moveRequestId: number) => `/sentEstimates`, // 기사
   reviewReceived: (moverId: number) => `/movers/${moverId}`, // 기사 //새로 만들긴해야할듯
 } as const;
